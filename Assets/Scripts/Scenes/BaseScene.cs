a49ae24fffe52060f0fd2893704ce8d4743954c2
dubidubob <2076397@ewhain.net>
2024-07-28 03:09:38 +0900

    add : scene manager

---
+++ Assets/Scripts/Scenes/BaseScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseScene : MonoBehaviour
+{
+    //Define.Scene _sceneType = Define.Scene.Unknown;
+    public Define.Scene SceneType { get; protected set; } = Define.Scene.Unknown;
+    void Start()
+    {
+    }
+
+    protected virtual void Init()
+    { 
+    }
+
+    public abstract void Clear();
+}
